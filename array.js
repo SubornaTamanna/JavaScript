5a0cf88f353416f74390ce9beeac5c3d2a098e69
SubornaTamanna <subornatamanna21@gmail.com>
2025-01-06 15:20:23 +0600

    array

---
+++ array.js
@@ -0,0 +1,36 @@
+// let number = [1,2,3,4,];
+// let last = number.pop()
+// console.log(number);
+
+
+
+
+
+// let number =[1,2,3,4,5];
+// number.pop()
+// console.log(number);
+
+
+
+// let snumber =[1,2,3,4,5];
+// let first = snumber.shift();
+// console.log(first)
+
+
+// let snumber =[1,2,3,4,5];
+// snumber.shift();
+// console.log(snumber)
+
+
+// let number =[1,2,3,4,5];
+// let newNumber =[];
+// newNumber.push(number.pop())
+// newNumber.push(number.pop())
+// console.log(number)
+// console.log(newNumber)
+
+
+let arr1 =["jobaida","jui"]
+let arr2 =["tamanna","suborna"]
+let combined =arr1.concat(arr2);
+console.log(combined);
